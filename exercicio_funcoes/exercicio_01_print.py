3164f2754dc04659e33d170fa318be88b9cdcd92
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-03-20 08:53:23 -0300

    Editando resposta para igualar com o que foi pedido

--- exercicio_funcoes/exercicio_01_print.py
+++ exercicio_funcoes/exercicio_01_print.py
@@ -3,29 +3,25 @@
 para um n informado pelo usuário. Use uma função que receba 
 um valor n inteiro e imprima até a n-ésima linha.
 """
-contador = 0
-
-lista_enesima = []
 # Entrada de dados
-def enesima(valor:int) -> int:   
-    global contador
-
+def enesima(valor:int) -> int:
+    lista_enesima = []
+     
     for index in range(valor):
-        lista = []
-        index += 1
-        for _ in range(index):
-            contador += 1
-            lista.append(contador)
-        
-        lista_enesima.append(lista)
-        contador=0   
-    
-        
-    
-    return  lista_enesima
-        
+        index += 1 
+        numero = 0
+        lista=[] 
+              
+        for numero in range(index):
+            numero += 1
+            lista.append(numero)
+      
+        lista_enesima.append(lista)        
+    return lista_enesima
 
-print(enesima(4))      
+resultado = enesima(int(input("Digite um valor: ")))
 
-            
+for indice,_ in enumerate(resultado):
+    print(resultado[indice])
+     
         
