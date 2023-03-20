2a8b7e508e65584f1c16229da6d08c6ece180a72
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-03-20 19:58:56 -0300

    Teste de argumento keyword

--- exercicio_funcoes/exercicio_01_print.py
+++ exercicio_funcoes/exercicio_01_print.py
@@ -8,6 +8,7 @@ def enesima(valor:int) -> int:
     lista_enesima = []
      
     for index in range(valor):
+        i = index
         index += 1 
         numero = 0
         lista=[] 
@@ -15,13 +16,13 @@ def enesima(valor:int) -> int:
         for numero in range(index):
             numero += 1
             lista.append(numero)
-      
-        lista_enesima.append(lista)        
+        lista_enesima.append(lista)
+           
     return lista_enesima
 
 resultado = enesima(int(input("Digite um valor: ")))
 
-for indice,_ in enumerate(resultado):
-    print(resultado[indice])
-     
+
+for item in resultado:
+    print(item)
         
