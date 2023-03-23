e073672750fc0ac366dc69fcc86d27231d6b1a67
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-03-23 20:00:27 -0300

    Doctest implementação

--- exercicio_poo/exercicio_02_quadrado.py
+++ exercicio_poo/exercicio_02_quadrado.py
@@ -1,26 +1,15 @@
-"""
-02 - Classe Quadrado: Crie uma classe que modele um quadrado:
-
+"""02 - Classe Quadrado: Crie uma classe que modele um quadrado:
 Atributos: Tamanho do lado
 Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+
+>>>#Teste de classe
+>>> quadra_escolar = Qudrado()
+>>> quadra_escolar.tamanho_do_lado(5)
+>>> quadra_escolar.retornar_valor_do_lado()
+>>> 5
+>>>quadra_escolar.mudar_valor_do_lado(7)
+>>> quadra_escolar.retornar_valor_do_lado()
+>>> 7
+>>>quadra_escolar.calcular_area()
+>>> 49
 """
-class MetroQuadrado:
-
-    def __init__(self,  lado):
-        self.tamanho_lado = lado
-
-    def mudar_valor_de_lado(self, novo_tamanho):
-        self.tamanho_lado = novo_tamanho
-
-    def calcular_area(self,  lado):
-        self.resultado = lado * lado
-        return  self.resultado
-
-    def retornar_valor_lado(self,):
-        return  self.tamanho_lado
-
-
-if  __name__   ==  ' __main__' :
-    quadra_escolar=MetroQuadrado(  )
-    quadra_escolar.tamanho_lado(5)
-    print(quadra_escolar.__dict__)
