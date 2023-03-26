28fbc3f107b190df1cdbf1e45860b9de00019ece
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-03-26 19:39:02 -0300

    Resolução dos exercicios 06 sobre strings

--- exercicio_string/exercicio_06.py
+++ exercicio_string/exercicio_06.py
@@ -4,5 +4,13 @@ a data com o nome do mês por extenso.
 """
 nascimento = input("Digite sua data de nascimento [ dd/mm/aaaa ] : ")
 
-meses = [ "Janeiro" , "Fevereiro" , "Março" , "Abril" , "Maio" , "Junho" , "Julho" , \
-          "Agosto" , "Setembro" , "Outubro"]
+meses = [ "Janeiro" , "Fevereiro" , "Março" , "Abril" , "Maio" , "Junho" , "Julho" , "Agosto" , "Setembro" , "Outubro" , "Novembro", "Dezembro"]
+
+dia = nascimento[:2]
+mes = int(nascimento[3:5])
+ano =  nascimento[6:]
+meses_str = meses[mes-1]
+
+print(f"Data de Nascimento: {nascimento}")
+print(f"Você nasceu em  {dia} de {meses_str} de {ano}. ")
+
