c13f888d276442916557291edd4623f361fdd5a8
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-01 17:24:51 -0300

    Exercicio com arquivo

---
+++ exercicio_com_arquivos/exercicio_01.py
@@ -0,0 +1,14 @@
+"""
+Faça um programa que leia um arquivo texto contendo uma lista de endereços IP e gere um outro arquivo, contendo um
+relatório dos endereços IP válidos e inválidos.
+O arquivo de entrada possui o seguinte formato:
+"""
+
+with open('relatorio.txt', 'r') as arquivo:
+    lista = arquivo.readlines()
+    print(type(lista))
+    print(lista)
+
+with open('relatorio_editado', 'w') as arquivo :
+    cabecalho = '[Endereços válidos:]'
+    arquivo.writelines(f'{cabecalho:}')
