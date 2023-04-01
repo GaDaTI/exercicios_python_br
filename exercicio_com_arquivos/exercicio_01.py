784af2d926625acd32849801cf0ca40b28b093b0
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-01 17:55:02 -0300

    Teste de validação de IP

--- exercicio_com_arquivos/exercicio_01.py
+++ exercicio_com_arquivos/exercicio_01.py
@@ -6,9 +6,24 @@ O arquivo de entrada possui o seguinte formato:
 
 with open('relatorio.txt', 'r') as arquivo:
     lista = arquivo.readlines()
-    print(type(lista))
-    print(lista)
+    #print(type(lista))
+    #print(lista)
 
 with open('relatorio_editado', 'w') as arquivo :
     cabecalho = '[Endereços válidos:]'
-    arquivo.writelines(f'{cabecalho:}')
+    arquivo.writelines(f'{cabecalho:}\n')
+    for ip_str in lista:
+        print(type(ip_str))
+        ip_numero = int(ip_str)
+        print(type(ip_numero))
+        #tamanho = len(ip_str.split('.'))
+        #if tamanho == 4:
+        #    if ip_numero >= 0 or ip_numero < 255:
+        #        arquivo.writelines(ip_str)
+
+        #print(tamanho)
+        #print(len(tamanho))
+        #print(type(tamanho))
+
+
+
